7de3b44057185a7d87f999f4279ad9b74ea60503
khanakitclass <khanakitclass@gmail.com>
2024-09-11 17:33:06 +0530

    done

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:4000/api"
+// export const BASE_URL = "http://localhost:4000/api"
 
-// export const BASE_URL = "https://solar-backend-teal.vercel.app/api"
+export const BASE_URL = "https://server11-omega.vercel.app/api"
